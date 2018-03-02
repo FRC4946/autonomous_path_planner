cf4bf089f3f73d7e5759e96ebb599175d4d6b034
Matthew Reynolds <rey.mtr@gmail.com>
2018-03-02 11:37:24 -0500

    Commented and cleaned Segment.java

--- src/ca/_4946/mreynolds/pathplanner/src/data/Segment.java
+++ src/ca/_4946/mreynolds/pathplanner/src/data/Segment.java
@@ -2,20 +2,42 @@ package ca._4946.mreynolds.pathplanner.src.data;
 
 import ca._4946.mreynolds.pathplanner.src.data.point.Point;
 
+/**
+ * A representation of pose and translational motion at a given moment, and the
+ * delta time between this and adjacent {@code Segment}s.
+ * <p>
+ * <b>Fields:</b>
+ * <li>Pose: x position, y position, heading
+ * <li>Motion: position travelled, velocity, acceleration, and jerk
+ * <li>Other: delta time
+ * 
+ * @author Matthew
+ *
+ */
 public class Segment {
 
-	public double pos;
-	public double vel;
-	public double accel;
-	public double jerk;
-	public double heading;
-	public double dt;
-	public double x;
-	public double y;
+	public double pos = 0;
+	public double vel = 0;
+	public double accel = 0;
+	public double jerk = 0;
+	public double heading = 0;
+	public double dt = 0;
+	public double x = 0;
+	public double y = 0;
 
+	/**
+	 * Create an empty segment with all parameters equal to 0
+	 */
 	public Segment() {
 	}
 
+	/**
+	 * Create a new {@code Segment} that clones all values from the specified
+	 * {@code Segment}
+	 * 
+	 * @param s
+	 *            the {@code Segment} to clone
+	 */
 	public Segment(Segment s) {
 		pos = s.pos;
 		vel = s.vel;
@@ -27,11 +49,31 @@ public class Segment {
 		y = s.y;
 	}
 
+	/**
+	 * Create a new {@code Segment} with all values equal to 0 except for
+	 * coordinates x and y, which are clones from the specified {@link Point}
+	 * 
+	 * @param p
+	 *            the {@code Point} to clone
+	 */
 	public Segment(Point p) {
 		this.x = p.getX();
 		this.y = p.getY();
 	}
 
+	/**
+	 * Create a new {@code Segment} with the specified parameters and all other
+	 * values equal to 0
+	 * 
+	 * @param pos
+	 *            the position
+	 * @param x
+	 *            the x coordinate
+	 * @param y
+	 *            the y coordinate
+	 * @param heading
+	 *            the heading
+	 */
 	public Segment(double pos, double x, double y, double heading) {
 		this.pos = pos;
 		this.x = x;
@@ -39,11 +81,17 @@ public class Segment {
 		this.heading = heading;
 	}
 
-	public void print() {
-		System.out.println(
-				pos + "\t" + vel + "\t" + accel + "\t" + jerk + "\t" + heading + "\t" + dt + "\t" + x + "\t" + y);
+	@Override
+	public String toString() {
+		return pos + "\t" + vel + "\t" + accel + "\t" + jerk + "\t" + heading + "\t" + dt + "\t" + x + "\t" + y;
 	}
 
+	/**
+	 * Create a {@link Point} representing the x and y coordinates of the
+	 * {@code Segment}
+	 * 
+	 * @return the {@code Point}
+	 */
 	public Point toPt() {
 		return new Point(x, y);
 	}
