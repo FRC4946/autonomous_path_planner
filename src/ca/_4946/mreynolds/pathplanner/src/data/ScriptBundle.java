883043247a1e6582af4036025440d67cd4b4eb67
Matthew Reynolds <rey.mtr@gmail.com>
2018-03-02 11:21:39 -0500

    Commented and cleaned ScriptBundle

--- src/ca/_4946/mreynolds/pathplanner/src/data/ScriptBundle.java
+++ src/ca/_4946/mreynolds/pathplanner/src/data/ScriptBundle.java
@@ -10,6 +10,13 @@ import ca._4946.mreynolds.pathplanner.src.data.actions.IntakeAction;
 import ca._4946.mreynolds.pathplanner.src.data.actions.OutputAction;
 import ca._4946.mreynolds.pathplanner.src.data.actions.TurnAction;
 
+/**
+ * A bundle of 4 {@link Script}s, one representing each of the 4 possible field
+ * configurations ("LLL", "LRL", "RLR", "RRR")
+ * 
+ * @author Matthew
+ *
+ */
 public class ScriptBundle {
 	public String name = "";
 	public String notes = "";
@@ -19,6 +26,14 @@ public class ScriptBundle {
 	public Script RL = new Script();
 	public Script RR = new Script();
 
+	/**
+	 * Get the {@link Script} for the specified field configuration
+	 * 
+	 * @param code
+	 *            the field config. Must be one of "ll", "lr", "rl", "rr"
+	 * @return the specified {@code Script}, or {@code null} if the specified
+	 *         {@code code} was invalid
+	 */
 	public Script getScript(String code) {
 		switch (code.toLowerCase()) {
 		case "ll":
@@ -30,17 +45,27 @@ public class ScriptBundle {
 		case "rr":
 			return RR;
 		}
-		
+
 		return null;
 	}
 
+	/**
+	 * @return a {@code Script} array {LL, LR, RL, RR}
+	 */
 	public Script[] asArray() {
 		return new Script[] { LL, LR, RL, RR };
 	}
 
+	/**
+	 * Determine if there are any parallel actions operating on the same subsystem
+	 * that would conflict in any of the 4 {@link Script}s in the bundle
+	 * 
+	 * @return {@code true} if there are no conflicts
+	 */
 	public boolean validateParallelActions() {
 		for (Script s : asArray()) {
 
+			// Add up all of the parallel actions preceding each sequential action
 			List<Class<?>> parallelActions = new ArrayList<>();
 			for (Action<?> a : s.getActions()) {
 				if (a.getBehaviour() == Action.Behaviour.kParallel) {
@@ -49,7 +74,8 @@ public class ScriptBundle {
 				}
 				parallelActions.add(a.getClass());
 
-				// Equate Drive&Turn, Intake&Output
+				// Equate Drive&Turn, Intake&Output actions since they operate on the same
+				// subsystem, and this allows for easy checking of subsystem conflicts
 				for (int i = 0; i < parallelActions.size(); i++) {
 					if (parallelActions.get(i) == TurnAction.class)
 						parallelActions.set(i, DriveAction.class);
@@ -57,17 +83,19 @@ public class ScriptBundle {
 						parallelActions.set(i, IntakeAction.class);
 				}
 
-				// Check
+				// Check if the actions conflict
+				// TODO: Allow parallel actions if they are delayed and not overlapping
 				for (Class<?> c : parallelActions) {
 					if (Collections.frequency(parallelActions, c) > 1)
 						return false;
 				}
 
+				// Clear the current list of parallel actions
 				parallelActions.clear();
 			}
 		}
 
+		// If we haven't run into any conflicts, return true
 		return true;
 	}
-
 }
