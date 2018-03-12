e9882fcc5649b572bb69d4e9f11fc7cae628058d
Matthew Reynolds <rey.mtr@gmail.com>
2018-03-12 08:59:33 -0400

    Commented and cleaned FileIO

--- src/ca/_4946/mreynolds/pathplanner/src/io/FileIO.java
+++ src/ca/_4946/mreynolds/pathplanner/src/io/FileIO.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.text.DecimalFormat;
+import java.util.Properties;
 
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -56,7 +57,8 @@ public class FileIO {
 	private static DecimalFormat f = new DecimalFormat("0.0#####");
 
 	/**
-	 * The default directory for scripts on the host machine
+	 * The default directory for scripts on the host machine. This is usually
+	 * {@code <Username>/Documents/AutoPathPlanner/}
 	 */
 	public static String DEFAULT_DIR = System.getProperty("user.home") + System.getProperty("file.separator")
 			+ "Documents" + System.getProperty("file.separator") + "AutoPathPlanner";
@@ -68,7 +70,7 @@ public class FileIO {
 	public static String FTP_PASSWORD = "";
 
 	/**
-	 * 
+	 * Ensure the {@link FileIO#DEFAULT_DIR} exists
 	 */
 	public static void createDefaultDir() {
 		File dir = new File(DEFAULT_DIR);
@@ -77,6 +79,16 @@ public class FileIO {
 			dir.mkdir();
 	}
 
+	/**
+	 * Load a {@link ScriptBundle} from the specified {@code XML} {@link File}
+	 * 
+	 * @param file
+	 *            the {@code XML File} to read
+	 * @return the read {@code ScriptBundle}
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 */
 	public static ScriptBundle loadScript(File file) throws ParserConfigurationException, SAXException, IOException {
 		ScriptBundle scripts = new ScriptBundle();
 
@@ -88,11 +100,12 @@ public class FileIO {
 		// http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
 		doc.getDocumentElement().normalize();
 
+		if (doc.getElementsByTagName("script").getLength() != 1)
+			throw new IOException("Invalid script file!");
+
+		// Load all of the data from the file
 		scripts.name = ((Element) doc.getElementsByTagName("script").item(0)).getAttribute("name");
 		scripts.notes = ((Element) doc.getElementsByTagName("script").item(0)).getAttribute("notes");
-
-		// // All of the nodes of the main element (the "script" element)
-		// NodeList nodeList = doc.getFirstChild().getChildNodes();
 
 		Element ll = (Element) doc.getElementsByTagName("ll").item(0);
 		Element lr = (Element) doc.getElementsByTagName("lr").item(0);
@@ -108,123 +121,149 @@ public class FileIO {
 		return scripts;
 	}
 
+	/**
+	 * Load a {@link Script} from the specified {@link Element}, assuming the
+	 * {@code Element} has a tag name equal to one of {@code ll}, {@code lr},
+	 * {@code rl}, or {@code rr}
+	 * 
+	 * @param parent
+	 *            the {@code Element} to read
+	 * 
+	 * @return the read {@code Script}
+	 */
 	@SuppressWarnings("unchecked")
 	private static Script loadScript(Element parent) {
 		Script s = new Script();
 
+		// Iterate through every action element
 		NodeList list = parent.getElementsByTagName("action");
-
 		for (int i = 0; i < list.getLength(); i++) {
-			if (list.item(i).getNodeType() == Node.ELEMENT_NODE) {
-				Element curEl = (Element) list.item(i);
+			if (list.item(i).getNodeType() != Node.ELEMENT_NODE)
+				continue;
+			Element curEl = (Element) list.item(i);
 
-				@SuppressWarnings("rawtypes")
-				Action curAction = null;
+			@SuppressWarnings("rawtypes")
+			Action curAction = null;
 
-				String type = curEl.getAttribute("type");
-				switch (type) {
-				case "Delay":
-					curAction = new DelayAction();
-					break;
-				case "Elevator":
-					curAction = new ElevatorAction();
-					break;
-				case "Arm":
-					curAction = new ArmAction();
-					break;
-				case "Intake":
-					curAction = new IntakeAction();
-					break;
-				case "Output":
-					curAction = new OutputAction();
-					break;
-				case "Drive":
-					curAction = new DriveAction();
-					break;
-				case "Turn":
-					curAction = new TurnAction();
-					break;
-				}
-
-				if (curAction == null)
-					continue;
-
-				try {
-					curAction.setOptions(
-							Enum.valueOf(curAction.getOption().getDeclaringClass(), curEl.getAttribute("options")));
-				} catch (IllegalArgumentException e) {
-					curAction.setOptions(curAction.getDefaultOption());
-				}
-
-				try {
-					curAction.setBehaviour(Enum.valueOf(curAction.getBehaviour().getDeclaringClass(),
-							curEl.getAttribute("behaviour")));
-				} catch (IllegalArgumentException e) {
-					curAction.setBehaviour(Behaviour.kSequential);
-				}
-				curAction.setDelay(Double.parseDouble(curEl.getAttribute("delay") + "0"));
-				curAction.setData(Double.parseDouble(curEl.getAttribute("data") + "0"));
-				curAction.setTimeout(Double.parseDouble(curEl.getAttribute("timeout") + "0"));
-
-				if (curAction instanceof DriveAction) {
-					// TODO: Change "waypoint" to "controlpoint"
-					NodeList controlpts = curEl.getElementsByTagName("waypoint");
-					for (int j = 0; j < controlpts.getLength(); j++) {
-						if (controlpts.item(j).getNodeType() == Node.ELEMENT_NODE) {
-
-							ControlPoint pt = new ControlPoint();
-							Element curPtEl = (Element) controlpts.item(j);
-
-							pt.setX(Double.parseDouble(curPtEl.getAttribute("x") + "0"));
-							pt.setY(Double.parseDouble(curPtEl.getAttribute("y") + "0"));
-							pt.setR(Double.parseDouble(curPtEl.getAttribute("radius") + "0"));
-							pt.setHeading(Double.parseDouble(curPtEl.getAttribute("heading") + "0"));
-							pt.setAutomaticHeading(curPtEl.getAttribute("autoHeading").contains("true"));
-							pt.setMagnet(curPtEl.getAttribute("magnet").contains("true"));
-
-							((DriveAction) curAction).addPt(pt);
-						}
-					}
-				}
-
-				s.addAction(curAction);
+			// Create the Action with the specified type
+			String type = curEl.getAttribute("type");
+			switch (type) {
+			case "Delay":
+				curAction = new DelayAction();
+				break;
+			case "Elevator":
+				curAction = new ElevatorAction();
+				break;
+			case "Arm":
+				curAction = new ArmAction();
+				break;
+			case "Intake":
+				curAction = new IntakeAction();
+				break;
+			case "Output":
+				curAction = new OutputAction();
+				break;
+			case "Drive":
+				curAction = new DriveAction();
+				break;
+			case "Turn":
+				curAction = new TurnAction();
+				break;
 			}
+
+			// If the type was invalid, skip this action and proceed to the next action in
+			// the script
+			if (curAction == null)
+				continue;
+
+			// Load the options
+			try {
+				curAction.setOptions(
+						Enum.valueOf(curAction.getOption().getDeclaringClass(), curEl.getAttribute("options")));
+			} catch (IllegalArgumentException e) {
+				curAction.setOptions(curAction.getDefaultOption());
+			}
+
+			// Load the behaviour
+			try {
+				curAction.setBehaviour(
+						Enum.valueOf(curAction.getBehaviour().getDeclaringClass(), curEl.getAttribute("behaviour")));
+			} catch (IllegalArgumentException e) {
+				curAction.setBehaviour(Behaviour.kSequential);
+			}
+
+			// Load the remaining action parameters
+			curAction.setDelay(Double.parseDouble(curEl.getAttribute("delay") + "0"));
+			curAction.setData(Double.parseDouble(curEl.getAttribute("data") + "0"));
+			curAction.setTimeout(Double.parseDouble(curEl.getAttribute("timeout") + "0"));
+
+			// If the action is a DriveAction, we need to load more data
+			if (curAction instanceof DriveAction) {
+
+				// Iterate through every waypoint element
+				NodeList controlpts = curEl.getElementsByTagName("waypoint"); // TODO: Change to "controlpoint"
+				for (int j = 0; j < controlpts.getLength(); j++) {
+					if (controlpts.item(j).getNodeType() != Node.ELEMENT_NODE)
+						continue;
+
+					// Load the point
+					ControlPoint pt = new ControlPoint();
+					Element curPtEl = (Element) controlpts.item(j);
+
+					pt.setX(Double.parseDouble(curPtEl.getAttribute("x") + "0"));
+					pt.setY(Double.parseDouble(curPtEl.getAttribute("y") + "0"));
+					pt.setR(Double.parseDouble(curPtEl.getAttribute("radius") + "0"));
+					pt.setHeading(Double.parseDouble(curPtEl.getAttribute("heading") + "0"));
+					pt.setAutomaticHeading(curPtEl.getAttribute("autoHeading").contains("true"));
+					pt.setMagnet(curPtEl.getAttribute("magnet").contains("true"));
+
+					((DriveAction) curAction).addPt(pt);
+				}
+
+			}
+
+			s.addAction(curAction);
 		}
 
 		return s;
+
 	}
 
-	public static void saveScript(ScriptBundle scBundle, File file)
+	/**
+	 * Save the specified {@link ScriptBundle} to the specified {@code XML}
+	 * {@link File}
+	 * 
+	 * @param bundle
+	 *            the {@code ScriptBundle} to write
+	 * @param file
+	 *            the {@code File} to write
+	 * @throws ParserConfigurationException
+	 * @throws TransformerException
+	 */
+	public static void saveScript(ScriptBundle bundle, File file)
 			throws ParserConfigurationException, TransformerException {
 
 		// Create a DocumentBuilder object, to build the XML file
 		DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 
-		// Create the root element "ll"
+		// Create the root element "script"
 		Document doc = docBuilder.newDocument();
 		Element root = doc.createElement("script");
 		doc.appendChild(root);
 
-		root.setAttribute("name", scBundle.name);
-		root.setAttribute("notes", scBundle.notes);
-
-		Element llElement = doc.createElement("ll");
-		root.appendChild(saveScript(doc, llElement, scBundle.LL, false));
+		// Load all the data into the script element
+		root.setAttribute("name", bundle.name);
+		root.setAttribute("notes", bundle.notes);
+		root.appendChild(saveScript(doc, "ll", bundle.LL, false));
+		root.appendChild(saveScript(doc, "lr", bundle.LR, false));
+		root.appendChild(saveScript(doc, "rl", bundle.RL, false));
+		root.appendChild(saveScript(doc, "rr", bundle.RR, false));
 
 		// for (DriveAction a : scBundle.LL.getDriveActions()) {
 		// PathParser.smoothAccelJerk(a.getLeftPath());
 		// PathParser.smoothAccelJerk(a.getRightPath());
 		// System.out.println(printPath(a));
 		// }
-
-		Element lrElement = doc.createElement("lr");
-		root.appendChild(saveScript(doc, lrElement, scBundle.LR, false));
-
-		Element rlElement = doc.createElement("rl");
-		root.appendChild(saveScript(doc, rlElement, scBundle.RL, false));
-
-		Element rrElement = doc.createElement("rr");
-		root.appendChild(saveScript(doc, rrElement, scBundle.RR, false));
 
 		// Write the content into an XML file
 		DOMSource source = new DOMSource(doc);
@@ -238,11 +277,29 @@ public class FileIO {
 
 	}
 
-	private static Element saveScript(Document doc, Element parentElement, Script sc, boolean isExport)
+	/**
+	 * Create an {@link Element} describing the specified {@link Script}
+	 * 
+	 * @param doc
+	 *            the resulting {@code Element}'s source {@link Document}
+	 * @param config
+	 *            the configuration this {@code Script} specifies. Must be one of
+	 *            {@code "ll"}, {@code "lr"}, {@code "rl"}, or {@code "rr"}
+	 * @param script
+	 *            the {@code Script} to read
+	 * @param isExport
+	 *            {@code true} if the resulting element will be uploaded to the
+	 *            robot rather than saved on the host PC
+	 * @return the generated {@code Element}
+	 * @throws ParserConfigurationException
+	 */
+	private static Element saveScript(Document doc, String config, Script script, boolean isExport)
 			throws ParserConfigurationException {
 
+		Element parentElement = doc.createElement(config);
+
 		// For every action in the script list
-		for (Action<?> a : sc.getActions()) {
+		for (Action<?> a : script.getActions()) {
 
 			// Create an element with called "Team"
 			Element curElement = doc.createElement("action");
@@ -298,81 +355,105 @@ public class FileIO {
 		return parentElement;
 	}
 
+	/**
+	 * Upload the specified {@link ScriptBundle} to the robot with the specified
+	 * {@link File} name
+	 * 
+	 * @param scBundle
+	 *            the {@code ScriptBundle} to upload
+	 * @param file
+	 *            the {@code File} name
+	 * @throws ParserConfigurationException
+	 * @throws TransformerException
+	 * @throws IOException
+	 */
 	public static void uploadScript(ScriptBundle scBundle, File file)
 			throws ParserConfigurationException, TransformerException, IOException {
 
 		// Create a DocumentBuilder object, to build the XML file
 		DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 
-		// Create the root element "ll"
+		// Create the root element "script"
 		Document doc = docBuilder.newDocument();
 		Element root = doc.createElement("script");
 		doc.appendChild(root);
 
+		// Load all the data into the script element
 		root.setAttribute("name", scBundle.name);
-
-		Element llElement = doc.createElement("ll");
-		root.appendChild(saveScript(doc, llElement, scBundle.LL, true));
-
-		Element lrElement = doc.createElement("lr");
-		root.appendChild(saveScript(doc, lrElement, scBundle.LR, true));
-
-		Element rlElement = doc.createElement("rl");
-		root.appendChild(saveScript(doc, rlElement, scBundle.RL, true));
-
-		Element rrElement = doc.createElement("rr");
-		root.appendChild(saveScript(doc, rrElement, scBundle.RR, true));
+		root.setAttribute("notes", scBundle.notes);
+		root.appendChild(saveScript(doc, "ll", scBundle.LL, true));
+		root.appendChild(saveScript(doc, "lr", scBundle.LR, true));
+		root.appendChild(saveScript(doc, "rl", scBundle.RL, true));
+		root.appendChild(saveScript(doc, "rr", scBundle.RR, true));
 
 		// Write the content into an XML file
 		DOMSource source = new DOMSource(doc);
 
+		// Display a status popup while we upload
 		StatusPopup popup = new StatusPopup("Uploading file", "");
 		EventQueue.invokeLater(() -> popup.setVisible(true));
 
+		// Spawn a thread to write to the robot over FTP. We use concurrency so as to
+		// not stall Swing
 		Thread ftpThread = new Thread(() -> {
+
+			// Open an FTP connection to the robot
 			FTPClient ftp = ftpConnect(popup);
-			if (ftp != null) {
+			if (ftp == null)
+				return;
 
-				// The OutputStream where the file will be written
-				OutputStream ftpOut = ftpOpen(ftp, popup, "script.xml");
-				StreamResult result = new StreamResult(ftpOut);
+			// The OutputStream where the file will be written
+			OutputStream ftpOut = ftpOpen(ftp, popup, "script.xml"); // TODO: Use the file parameter
+			StreamResult result = new StreamResult(ftpOut);
 
-				// The object used to build the XML document into the file
-				try {
-					Transformer transformer = TransformerFactory.newInstance().newTransformer();
-					transformer.transform(source, result);
-				} catch (TransformerFactoryConfigurationError | TransformerException e) {
-					e.printStackTrace();
-				}
-
-				try {
-					ftpOut.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				ftpDisconnect(ftp, popup);
+			// The object used to build the XML document into the file
+			try {
+				Transformer transformer = TransformerFactory.newInstance().newTransformer();
+				transformer.transform(source, result);
+			} catch (TransformerFactoryConfigurationError | TransformerException e) {
+				e.printStackTrace();
 			}
+
+			// Close the FTP connection
+			try {
+				ftpOut.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			ftpDisconnect(ftp, popup);
 		});
 		ftpThread.start();
-
 	}
 
+	/**
+	 * Open an FTP connection with the robot
+	 * 
+	 * @param log
+	 *            the {@link StatusPopup} to output the log to
+	 * @return the connected {@link FTPClient}
+	 */
 	private static FTPClient ftpConnect(StatusPopup log) {
 		FTPClient ftp = new FTPClient();
 		FTPClientConfig config = new FTPClientConfig();
 
-		// config.setXXX(YYY); // change required options
-		// for example config.setServerTimeZoneId("Pacific/Pitcairn")
-
 		ftp.configure(config);
 		int reply;
-
 		try {
 
+			// Set the timeout
+			// TODO: This doesn't work
+			Properties env = System.getProperties();
+			env.put("com.example.jndi.dns.recursion", "false");
+			env.put("com.example.jndi.dns.timeout.initial", "500");
+			env.put("com.example.jndi.dns.timeout.retries", "1");
+
+			// Connect to the server
 			log.append("Establishing connection...", "Connecting");
+			ftp.setDefaultTimeout(1000);
 			ftp.connect(FTP_SERVER);
 			log.append(ftp.getReplyString());
 
+			// Log in
 			log.append("Logging in as user " + FTP_USERNAME + "...");
 			ftp.login(FTP_USERNAME, FTP_PASSWORD);
 			log.append(ftp.getReplyString());
@@ -400,15 +481,27 @@ public class FileIO {
 		return ftp;
 	}
 
+	/**
+	 * Open an {@link OutputStream} on the FTP server where we can write files
+	 * 
+	 * @param ftp
+	 *            the {@link FTPClient}
+	 * @param log
+	 *            the {@link StatusPopup} to output the log to
+	 * @param filename
+	 *            the name of the file to write
+	 * @return the {@code OutputStream} to the new file, or null if an error
+	 *         occurred
+	 */
 	private static OutputStream ftpOpen(FTPClient ftp, StatusPopup log, String filename) {
-
 		try {
 
+			// Go to the user home
 			log.append("Going to dir /home/lvuser", "Uploading file");
 			ftp.changeWorkingDirectory("/home/lvuser");
 			log.append(ftp.getReplyString());
 
-			// Check if directory exists
+			// Check if our directory exists
 			log.append("Checking if directory AutoPathPlanner exists");
 			if (ftp.cwd("AutoPathPlanner") == 550) { // 250=exists
 
@@ -436,6 +529,7 @@ public class FileIO {
 			ftp.changeWorkingDirectory("AutoPathPlanner");
 			log.append(ftp.getReplyString());
 
+			// Delete the old version of the file
 			ftp.deleteFile(filename);
 
 			// Upload the file
@@ -455,9 +549,16 @@ public class FileIO {
 		}
 
 		return null;
-
 	}
 
+	/**
+	 * Close the FTP stream and connection
+	 * 
+	 * @param ftp
+	 *            the {@link FTPClient} to close
+	 * @param log
+	 *            the {@link StatusPopup} to output the log to
+	 */
 	private static void ftpDisconnect(FTPClient ftp, StatusPopup log) {
 		try {
 			log.append("Closing file");
@@ -479,6 +580,13 @@ public class FileIO {
 		}
 	}
 
+	/**
+	 * Get every segment of the path pair in a formatted string
+	 * 
+	 * @param a
+	 *            the {@link DriveAction} to parse
+	 * @return the formatted string
+	 */
 	private static String printPath(DriveAction a) {
 
 		String path = "";
@@ -508,6 +616,10 @@ public class FileIO {
 		return path;
 	}
 
+	/**
+	 * @return a {@link JFileChooser} filtering for {@code XML} files in the
+	 *         {@link FileIO#DEFAULT_DIR}
+	 */
 	public static JFileChooser getFileChooser() {
 
 		FileIO.createDefaultDir();
@@ -519,6 +631,12 @@ public class FileIO {
 		return fileChooser;
 	}
 
+	/**
+	 * @param defaultFile
+	 *            the default file to select
+	 * @return a {@link JFileChooser} filtering for {@code XML} files in the
+	 *         {@link FileIO#DEFAULT_DIR}
+	 */
 	public static JFileChooser getFileChooser(String defaultFile) {
 		JFileChooser fc = getFileChooser();
 		fc.setSelectedFile(new File(defaultFile));
